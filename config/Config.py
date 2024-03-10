f20ed4f9061dba2fe329c9591d8c6ac4c779adad
Erica M. ("Loonatic") <l.oony@aol.com>
2024-03-09 23:03:10 -0600

    Config: Update remote database host config

--- config/Config.py
+++ config/Config.py
@@ -8,7 +8,7 @@ class Config:
     REMOTE_USERNAME: str = "admin"
     REMOTE_PASSWORD: str = "***REMOVED***"
     REMOTE_HOST: str = "***REMOVED***"
-    SCHEMA: str = "InventoryDatabase"
+    SCHEMA: str = "DaycareDatabase"
 
     # Will output active transactions being made to the database in console
     VERBOSE_OUTPUT: bool = True
@@ -16,7 +16,7 @@ class Config:
     UUID_TOKEN_LENGTH: int = 32
 
     # LOCAL_FILE, LOCAL_MEMORY, REMOTE_SERVER
-    CONNECTION_MODE: ConnectionType = ConnectionType.LOCAL_FILE
+    CONNECTION_MODE: ConnectionType = ConnectionType.REMOTE_SERVER
 
     LOCAL_FILENAME: str = "data.db"
 
